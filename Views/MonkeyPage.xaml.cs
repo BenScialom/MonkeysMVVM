0a80a1096e5dd4438b94d5bb45d73b7d623a9b8d
User <User@LAB3-3>
2024-01-29 15:32:07 +0200

    New

---
+++ Views/MonkeyPage.xaml.cs
@@ -0,0 +1,12 @@
+using MonkeysMVVM.ViewModels;
+namespace MonkeysMVVM.Views;
+
+
+public partial class MonkeyPage : ContentPage
+{
+	public MonkeyPage()
+	{
+		InitializeComponent();
+		this.BindingContext = new MonkeyPageViewModel();
+	}
+}
