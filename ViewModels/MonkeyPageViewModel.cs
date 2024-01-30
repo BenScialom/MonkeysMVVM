9daf9019c7105362d9a60afcca87a76328dfb41d
User <User@LAB3-3>
2024-01-30 14:54:17 +0200

    New application

--- ViewModels/MonkeyPageViewModel.cs
+++ ViewModels/MonkeyPageViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MonkeysMVVM.Models;
+using System.Windows.Input;
 
 namespace MonkeysMVVM.ViewModels
 {
-    internal class MonkeyPageViewModel
+    public class MonkeyPageViewModel : ViewModel
     {
+        private string country;
+        private int count;
+        public string Country { get { return country; } set { country = value; OnPropertyChanged();((Command)SearchByCountryCommand).ChangeCanExecute(); } }
+        public int Count { get { return count; } set { count = value; OnPropertyChanged(); } }
+        public ICommand SearchByCountryCommand { get; set; }
+        private Monkey monkey;
+        public string Name { get { return monkey.Name; } }
+        public string ImageUrl {  get { return monkey.ImageUrl; } }
+        public MonkeyPageViewModel()
+        {
+            monkey = new Monkey() { Name = "No Monkeys right now" };
+            SearchByCountryCommand = new Command(FindMonkeys,()=>Country!=null);
+        }
+
+        private void FindMonkeys()
+        {
+           
+        }
     }
 }
